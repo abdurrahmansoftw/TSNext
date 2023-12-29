2d05025c9552bb427f41f3fdae45b363110529f5
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-29 23:51:49 +0600

    static and dynamic rendaring

--- practics/app/users/page.tsx
+++ practics/app/users/page.tsx
@@ -14,13 +14,17 @@ const UsersPage = async () => {
   })
   const users: User[] = await res.json()
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>
-          <Link href={`/users/${user.id}`}>{user.name}</Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <h1>users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link href={`/users/${user.id}`}>{user.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
 
