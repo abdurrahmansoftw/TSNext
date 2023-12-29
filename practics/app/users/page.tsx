55ed070970777d61f2707ca3698afabb7d3da5f2
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-29 23:45:18 +0600

    fetch

--- practics/app/users/page.tsx
+++ practics/app/users/page.tsx
@@ -1,10 +1,24 @@
 import Link from 'next/link'
 
-const UsersPage = () => {
+interface User {
+  id: number
+  name: string
+  email: string
+  phone: string
+  website: string
+}
+
+const UsersPage = async () => {
+  const res = await fetch('http://jsonplaceholder.typicode.com/users')
+  const users: User[] = await res.json()
   return (
-    <div>
-      <Link href='/users/new'>New User</Link>
-    </div>
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          <Link href={`/users/${user.id}`}>{user.name}</Link>
+        </li>
+      ))}
+    </ul>
   )
 }
 
