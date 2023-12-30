da3ef2cac92e80388f67a3d9120939e072bcb8c7
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-30 22:08:40 +0600

    add admin

---
+++ practics/app/admin/layout.tsx
@@ -0,0 +1,33 @@
+'use client'
+import { Box, Grid, Paper } from '@mui/material'
+import { styled } from '@mui/material/styles'
+import { ReactNode } from 'react'
+
+interface Props {
+  children: ReactNode
+}
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}))
+
+const AdminLayout = ({ children }: Props) => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <Item>Sidebar</Item>
+        </Grid>
+        <Grid item xs={8}>
+          <Item> {children} </Item>
+        </Grid>
+      </Grid>
+    </Box>
+  )
+}
+
+export default AdminLayout
