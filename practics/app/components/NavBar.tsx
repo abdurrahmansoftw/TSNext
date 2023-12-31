19473d63c6deca64f7f0d929360a6de1d8dabeba
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-31 23:59:29 +0600

    admin remove

--- practics/app/components/NavBar.tsx
+++ practics/app/components/NavBar.tsx
@@ -18,9 +18,7 @@ const NavBar = () => {
           <Typography variant='h6' sx={{ flexGrow: 1 }} component={Link} href='/'>
             Home
           </Typography>
-          <Button color='inherit' component={Link} href='/admin'>
-            Admin
-          </Button>
+          
           <Button color='inherit' component={Link} href='/users'>
             Users
           </Button>
