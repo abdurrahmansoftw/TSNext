b53dbc41ab18300c21d5fc4e5e497a9b58037d2f
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-31 18:17:40 +0600

    only user

--- practics/app/api/users/[id]/route.tsx
+++ practics/app/api/users/[id]/route.tsx
@@ -1,12 +1,11 @@
+import prisma from '@/prisma/client'
 import { NextRequest, NextResponse } from 'next/server'
 import schema from '../schema'
 
 export async function GET(request: NextRequest, { params }: { params: { id: number } }) {
-  if (params.id > 10) {
-    return NextResponse.json({ error: 'Invalid ID' }, { status: 404 })
-  }
-
-  return NextResponse.json({ id: 1, name: 'John Doe' })
+  const user = await prisma.user.findUnique({ where: { id: params.id } })
+  if (!user) return NextResponse.json({ error: 'User not found' }, { status: 404 })
+  return NextResponse.json(user)
 }
 
 export async function PUT(request: NextRequest, { params }: { params: { id: number } }) {
