7e7a2179341d34ed154cec511803ffbf6f8d9ef9
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-31 23:40:55 +0600

    get token

---
+++ practics/app/api/auth/token/route.ts
@@ -0,0 +1,6 @@
+import { getToken } from 'next-auth/jwt'
+import { NextRequest } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const token = await getToken({ req: request })
+}
