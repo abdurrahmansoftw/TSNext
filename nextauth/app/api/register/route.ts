cb746d3b0f67a79e3f23d5e1b86537434226fc92
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-01 15:25:37 +0600

    auth done

--- nextauth/app/api/register/route.ts
+++ nextauth/app/api/register/route.ts
@@ -5,25 +5,20 @@ import { z } from 'zod'
 
 const schema = z.object({
   email: z.string().email(),
-  password: z.string().min(3),
+  password: z.string().min(5),
 })
 
 export async function POST(request: NextRequest) {
   const body = await request.json()
-
+  
   const validation = schema.safeParse(body)
-  if (!validation.success) {
-    return NextResponse.json(validation.error.errors), { status: 400 }
-  }
+  if (!validation.success) return NextResponse.json(validation.error.errors, { status: 400 })
 
   const user = await prisma.user.findUnique({ where: { email: body.email } })
-  
-  if (user) {
-    return NextResponse.json({ message: 'Email already exists' }), { status: 400 }
-  }
+  if (user) return NextResponse.json({ error: 'User already exists' }, { status: 400 })
 
   const hashedPassword = await bcrypt.hash(body.password, 10)
   const newUser = await prisma.user.create({ data: { email: body.email, hashedPassword } })
 
-  return NextResponse.json({ email: newUser.email }, { status: 201 })
+  return NextResponse.json({ email: newUser.email })
 }
