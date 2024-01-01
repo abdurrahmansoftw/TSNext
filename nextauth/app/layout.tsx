8276500c64e400e96c76a2f38853862d746ba2f4
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-01 13:28:41 +0600

    navbar add

--- nextauth/app/layout.tsx
+++ nextauth/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import AuthProvider from './auth/Provider'
+import NavBar from './components/NavBar'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -14,7 +15,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <AuthProvider>{children}</AuthProvider>
+        <AuthProvider>
+          <NavBar />
+          {children}
+        </AuthProvider>
       </body>
     </html>
   )
