a96fbeadca0aa96493fa17cd413e14b10f8934c6
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-17 11:39:34 +0600

    Refactoring

--- nextauth/app/page.tsx
+++ nextauth/app/page.tsx
@@ -2,12 +2,12 @@ import { getServerSession } from 'next-auth'
 import { authOptions } from './api/auth/[...nextauth]/route'
 
 const page = async () => {
-  const session = await getServerSession(authOptions)
-  return (
-    <div>
-      <h1>User : {session && session.user!.name}</h1>
-    </div>
-  )
+	const session = await getServerSession(authOptions)
+	return (
+		<div>
+			<h1>User : {session && session.user!.name}</h1>
+		</div>
+	)
 }
 
 export default page
