189a38355ccb515c11eed3601d1eebf4a6eacce0
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-17 11:41:43 +0600

    Refactoring

--- practics/app/components/AddToCart.tsx
+++ practics/app/components/AddToCart.tsx
@@ -2,13 +2,13 @@
 import { Button } from '@mui/material'
 
 const AddToCart = () => {
-  return (
-    <div>
-      <Button onClick={() => console.log('Do something!')} variant='outlined'>
-        Add To Cart
-      </Button>
-    </div>
-  )
+	return (
+		<div>
+			<Button onClick={() => console.log('Do something!')} variant='outlined'>
+				Add To Cart
+			</Button>
+		</div>
+	)
 }
 
 export default AddToCart
