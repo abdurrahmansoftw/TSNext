9064489464f45213611da5598351210487ae0d72
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-17 11:38:12 +0600

    Refactoring

--- nextauth/app/components/NavBar.tsx
+++ nextauth/app/components/NavBar.tsx
@@ -6,37 +6,42 @@ import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 
 const NavBar = () => {
-  const { status, data: session } = useSession()
-  return (
-    <AppBar position='static'>
-      <Container maxWidth='xl'>
-        <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography variant='h6' component={Link} href={'/'} sx={{ flexGrow: 1 }}>
-            AKAID
-          </Typography>
+	const { status, data: session } = useSession()
+	return (
+		<AppBar position='static'>
+			<Container maxWidth='xl'>
+				<Toolbar disableGutters>
+					<AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+					<AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+					<Typography
+						variant='h6'
+						component={Link}
+						href={'/'}
+						sx={{ flexGrow: 1 }}
+					>
+						AKAID
+					</Typography>
 
-          {status === 'loading' && <p>Loading...</p>}
+					{status === 'loading' && <p>Loading...</p>}
 
-          {status === 'authenticated' && (
-            <div>
-              {session.user!.name}
-              <Button color='inherit' component={Link} href='/api/auth/signout'>
-                Logout
-              </Button>
-            </div>
-          )}
+					{status === 'authenticated' && (
+						<div>
+							{session.user!.name}
+							<Button color='inherit' component={Link} href='/api/auth/signout'>
+								Logout
+							</Button>
+						</div>
+					)}
 
-          {status === 'unauthenticated' && (
-            <Button color='inherit' component={Link} href='/api/auth/signin'>
-              Login
-            </Button>
-          )}
-        </Toolbar>
-      </Container>
-    </AppBar>
-  )
+					{status === 'unauthenticated' && (
+						<Button color='inherit' component={Link} href='/api/auth/signin'>
+							Login
+						</Button>
+					)}
+				</Toolbar>
+			</Container>
+		</AppBar>
+	)
 }
 
 export default NavBar
--- nextauth/app/error.tsx
+++ nextauth/app/error.tsx
@@ -1,21 +1,21 @@
 'use client'
 
 interface Props {
-  error: Error
-  reset: () => void
+	error: Error
+	reset: () => void
 }
 
 const ErrorPage = ({ error, reset }: Props) => {
-  console.log('Error', error)
+	console.log('Error', error)
 
-  return (
-    <>
-      <div>An unexpected error has occurred.</div>
-      <button className='btn' onClick={() => reset()}>
-        Retry
-      </button>
-    </>
-  )
+	return (
+		<>
+			<div>An unexpected error has occurred.</div>
+			<button className='btn' onClick={() => reset()}>
+				Retry
+			</button>
+		</>
+	)
 }
 
 export default ErrorPage
