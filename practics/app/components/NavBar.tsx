0880db8d1c029ce8b329fa75ec7ac6e57b928a19
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-17 11:41:57 +0600

    Refactoring

--- practics/app/components/NavBar.tsx
+++ practics/app/components/NavBar.tsx
@@ -1,49 +1,67 @@
 'use client'
 import MenuIcon from '@mui/icons-material/Menu'
-import { AppBar, Box, Button, IconButton, Toolbar, Typography } from '@mui/material'
+import {
+	AppBar,
+	Box,
+	Button,
+	IconButton,
+	Toolbar,
+	Typography,
+} from '@mui/material'
 import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 
 const NavBar = () => {
-  const { status, data: session } = useSession()
+	const { status, data: session } = useSession()
 
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position='sticky' elevation={1} color='primary'>
-        <Toolbar>
-          <IconButton size='large' edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant='h6' sx={{ flexGrow: 1 }} component={Link} href='/'>
-            Home
-          </Typography>
-          <Button color='inherit' component={Link} href='/admin'>
-            Admin
-          </Button>
-          <Button color='inherit' component={Link} href='/users'>
-            Users
-          </Button>
+	return (
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position='sticky' elevation={1} color='primary'>
+				<Toolbar>
+					<IconButton
+						size='large'
+						edge='start'
+						color='inherit'
+						aria-label='menu'
+						sx={{ mr: 2 }}
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography
+						variant='h6'
+						sx={{ flexGrow: 1 }}
+						component={Link}
+						href='/'
+					>
+						Home
+					</Typography>
+					<Button color='inherit' component={Link} href='/admin'>
+						Admin
+					</Button>
+					<Button color='inherit' component={Link} href='/users'>
+						Users
+					</Button>
 
-          {status === 'loading' && <div> Loading... </div>}
+					{status === 'loading' && <div> Loading... </div>}
 
-          {status === 'authenticated' && (
-            <div>
-              <Button color='inherit'>{session.user!.name}</Button>
-              <Button color='inherit' component={Link} href='/api/auth/signout'>
-                Logout
-              </Button>
-            </div>
-          )}
+					{status === 'authenticated' && (
+						<div>
+							<Button color='inherit'>{session.user!.name}</Button>
+							<Button color='inherit' component={Link} href='/api/auth/signout'>
+								Logout
+							</Button>
+						</div>
+					)}
 
-          {status === 'unauthenticated' && (
-            <Button color='inherit' component={Link} href='/api/auth/signin'>
-              Login
-            </Button>
-          )}
-        </Toolbar>
-      </AppBar>
-    </Box>
-  )
+					{status === 'unauthenticated' && (
+						<Button color='inherit' component={Link} href='/api/auth/signin'>
+							Login
+						</Button>
+					)}
+				</Toolbar>
+			</AppBar>
+		</Box>
+	)
 }
 
 export default NavBar
