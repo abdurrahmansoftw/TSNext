b11af2890fbb9510a438d752ae588e0bbd02f7bd
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-17 11:42:28 +0600

    Refactoring

--- practics/app/upload/page.tsx
+++ practics/app/upload/page.tsx
@@ -4,41 +4,51 @@ import { CldImage, CldUploadWidget } from 'next-cloudinary'
 import { useState } from 'react'
 
 interface UploadPageProps {
-  public_id: string
+	public_id: string
 }
 
 const UploadPage = () => {
-  const [publicId, setPublicid] = useState('')
+	const [publicId, setPublicid] = useState('')
 
-  return (
-    <>
-      {publicId && <CldImage width='270' height='180' src={publicId} sizes='100vw' alt='Description of my image' />}
-      <CldUploadWidget
-        options={{
-          sources: ['local'],
-          multiple: false,
-          maxFiles: 1,
-          resourceType: 'image',
-        }}
-        uploadPreset='nvmtnryr'
-        onUpload={(result, widget) => {
-          if (result.event !== 'success') return
-          const info = result.info as UploadPageProps
-          setPublicid(info.public_id)
-        }}>
-        {({ open }) => (
-          <Button
-            variant='outlined'
-            color='primary'
-            onClick={() => {
-              open()
-            }}>
-            Upload files
-          </Button>
-        )}
-      </CldUploadWidget>
-    </>
-  )
+	return (
+		<>
+			{publicId && (
+				<CldImage
+					width='270'
+					height='180'
+					src={publicId}
+					sizes='100vw'
+					alt='Description of my image'
+				/>
+			)}
+			<CldUploadWidget
+				options={{
+					sources: ['local'],
+					multiple: false,
+					maxFiles: 1,
+					resourceType: 'image',
+				}}
+				uploadPreset='nvmtnryr'
+				onUpload={(result, widget) => {
+					if (result.event !== 'success') return
+					const info = result.info as UploadPageProps
+					setPublicid(info.public_id)
+				}}
+			>
+				{({ open }) => (
+					<Button
+						variant='outlined'
+						color='primary'
+						onClick={() => {
+							open()
+						}}
+					>
+						Upload files
+					</Button>
+				)}
+			</CldUploadWidget>
+		</>
+	)
 }
 
 export default UploadPage
