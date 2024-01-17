028229c10a4b3c36e558dfb7bcec2e0be9cc5324
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-17 11:43:04 +0600

    UsersPage

--- practics/app/users/page.tsx
+++ practics/app/users/page.tsx
@@ -2,16 +2,16 @@ import UserTable from '../components/UserTable'
 import NewUserPage from './new/page'
 
 interface Props {
-  searchParams: { sortOder: string }
+	searchParams: { sortOder: string }
 }
 
 const UsersPage = async ({ searchParams: { sortOder } }: Props) => {
-  return (
-    <>
-      <NewUserPage />
-      <UserTable sortOder={sortOder} />
-    </>
-  )
+	return (
+		<>
+			<NewUserPage />
+			<UserTable sortOder={sortOder} />
+		</>
+	)
 }
 
 export default UsersPage
