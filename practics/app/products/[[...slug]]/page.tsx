d15cf2b27e0ce360d8d8f9a0000a642189319216
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-17 11:42:20 +0600

    Refactoring

--- practics/app/products/[[...slug]]/page.tsx
+++ practics/app/products/[[...slug]]/page.tsx
@@ -1,14 +1,17 @@
 interface Props {
-  params: { slug: string[] }
-  searchParams: { sortOder: string }
+	params: { slug: string[] }
+	searchParams: { sortOder: string }
 }
 
-const ProductPage = ({ params: { slug }, searchParams: { sortOder } }: Props) => {
-  return (
-    <div>
-      ProductPage {slug} : {sortOder}
-    </div>
-  )
+const ProductPage = ({
+	params: { slug },
+	searchParams: { sortOder },
+}: Props) => {
+	return (
+		<div>
+			ProductPage {slug} : {sortOder}
+		</div>
+	)
 }
 
 export default ProductPage
