f448434f9d1d6f313b20a14e94a2a91c5851f5c6
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-17 11:42:56 +0600

    ErrorPage

--- practics/app/users/error.tsx
+++ practics/app/users/error.tsx
@@ -1,6 +1,7 @@
 'use client'
+
 const ErrorPage = () => {
-  return <div>An error occurred while trying to render the page.</div>
+	return <div>An error occurred while trying to render the page.</div>
 }
 
 export default ErrorPage
