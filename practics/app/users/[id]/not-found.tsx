c7a4aacf58cb0487659bd8f368da976260c869bf
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-17 11:42:39 +0600

    UserDetailPage

--- practics/app/users/[id]/not-found.tsx
+++ practics/app/users/[id]/not-found.tsx
@@ -1,10 +1,10 @@
 const UserNotFoundPage = () => {
-  return (
-    <div>
-      <h1>404</h1>
-      <p>User not found</p>
-    </div>
-  )
+	return (
+		<div>
+			<h1>404</h1>
+			<p>User not found</p>
+		</div>
+	)
 }
 
 export default UserNotFoundPage
--- practics/app/users/[id]/page.tsx
+++ practics/app/users/[id]/page.tsx
@@ -1,14 +1,14 @@
 import { notFound } from 'next/navigation'
 
 interface Props {
-  params: { id: number }
+	params: { id: number }
 }
 
 const UserDetailPage = ({ params: { id } }: Props) => {
-  if (id > 10) {
-    return notFound()
-  }
-  return <div>UserDetailPage {id}</div>
+	if (id > 10) {
+		return notFound()
+	}
+	return <div>UserDetailPage {id}</div>
 }
 
 export default UserDetailPage
